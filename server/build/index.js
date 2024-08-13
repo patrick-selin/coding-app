e3a4c08ff60652e4ccc09712f38f5104bc1d396b
Patrick Selin <patrick.selin@gmail.com>
2024-08-13 11:31:52 +0300

    bootstrap an express app and add eslint

---
+++ server/build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// index.ts
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const server_1 = __importDefault(require("./server"));
+const PORT = process.env.PORT;
+server_1.default.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,18 @@
 // server.js
+
+import express, { Application } from "express";
+
+const app: Application = express();
+
+//
+// middleware
+app.use(express.json());
+
+//
+//routes
+app.get("/ping", (_req, res) => {
+  console.log("testing pinging...");
+  res.send("pong");
+});
+
+export default app;
