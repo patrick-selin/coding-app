46fedbc9c744470deafcf8551b1f6b9143b8e1c3
Patrick Selin <patrick.selin@gmail.com>
2024-08-15 13:55:25 +0300

    add endpoint to GET testItem with id param

--- server/src/controllers/testController.ts
+++ server/src/controllers/testController.ts
@@ -1,8 +1,16 @@
 // testController.js
 import { Request, Response } from "express";
-import { getAllTestItems } from "../services/testService";
+import { getAllTestItems, getTestItemById } from "../services/testService";
 
-export const getTestItems = (_req: Request, res: Response): void => {
-  const items = getAllTestItems();
-  res.json(items);
+export const getTestItems = (_req: Request, res: Response) => {
+  try {
+    const items = getAllTestItems();
+    res.json(items);
+  } catch (_error) {
+    res.status(500).json({ error: "Error" });
+  }
 };
+
+export const getTestItem = () => {
+    // todo
+}
--- server/src/services/testService.ts
+++ server/src/services/testService.ts
@@ -4,3 +4,7 @@ import { TestItem, testItems } from "../models/testItemModel";
 export const getAllTestItems = (): TestItem[] => {
   return testItems;
 };
+
+export const getTestItemById = (id: string): TestItem | undefined => {
+  return testItems.find((item) => item.id === id);
+};
