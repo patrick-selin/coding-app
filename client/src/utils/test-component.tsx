098c92e156d5210c821304ff2ed6c26582d6bfbc
Patrick Selin <patrick.selin@gmail.com>
2024-08-19 13:31:59 +0300

    add test button component that generates random number

--- client/src/utils/test-component.tsx
+++ client/src/utils/test-component.tsx
@@ -1,9 +1,24 @@
-const TestComponent = () => {
-    return (
-        <>
-          <div>test component</div>
-        </>
-      );
+import React, { useState } from "react";
+
+
+const TestComponent: React.FC = () => {
+  const [randomNumber, setRandomNumber] = useState<number | null>(null);
+
+  const generateRandomNumber = (): number => {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    setRandomNumber(randomNum);
+    return randomNum;
+  };
+
+  return (
+    <>
+      <br />
+      <br />
+      <div>Test Component</div>
+      <button onClick={generateRandomNumber}>Generate Random Number</button>
+      {randomNumber !== null && <div>Random Number: {randomNumber}</div>}
+    </>
+  );
 };
 
 export default TestComponent;
