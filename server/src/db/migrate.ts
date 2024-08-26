76a5f413aa734be9ef874ad7939d5883828c7b28
Patrick Selin <patrick.selin@gmail.com>
2024-08-26 13:26:32 +0300

    fix db connection issue in docker compose

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -1,24 +1,24 @@
 // migrate.ts
-import "dotenv/config";
 import postgres from "postgres";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
-import drizzleConfig from '../drizzle.config';
+import { drizzle } from "drizzle-orm/postgres-js";
+import "dotenv/config";
 
-const DATABASE_URL = process.env.POSTGRES_DB_URL;
+const databaseUrl = process.env.DATABASE_URL as string;
 
-if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
+console.log(`Connecting to database: ${process.env.POSTGRES_DB}`);
 
-const migrationClient = postgres(DATABASE_URL, { max: 1 });
-const db: PostgresJsDatabase = drizzle(migrationClient);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const migrationClient = postgres(databaseUrl, {
+  max: 1,
+});
 
-const main = async () => {
+async function main() {
   console.log("Migrating database...");
-  await migrate(db, { migrationsFolder: drizzleConfig.out });
+  await migrate(drizzle(migrationClient), { migrationsFolder: "./migrations" });
+
   await migrationClient.end();
   console.log("Database migrated successfully!");
-};
+}
 
 main();
