6c4e3b98ac054d5ca23721ed1bda6c5972a86634
Patrick Selin <patrick.selin@gmail.com>
2024-08-26 13:36:02 +0300

    fix db connection issue in docker compose2

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,24 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-// import { Pool } from "pg";
 import * as schema from "./schema";
 import "dotenv/config";
 
-// const pool = new Pool({
-//   host: process.env.POSTGRES_HOST,
-//   port: Number(process.env.POSTGRES_PORT),
-//   user: process.env.POSTGRES_USER,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: process.env.POSTGRES_DB,
-// });
-
-// const db = drizzle(pool);
-
-// export default db;
-const juu =
-  "postgres://patrick:jlafei8910fQQ-Hw92.W1l@127.0.0.1:5432/coding-app-db";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const queryClient = postgres(juu);
-// const queryClient = postgres(process.env.DATABASE_URL);
+const queryClient = postgres(process.env.DATABASE_URL as string);
 
 export const db = drizzle(queryClient, { schema, logger: true });
