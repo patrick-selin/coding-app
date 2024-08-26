033103f6940fb5e43a480409737ff5868d140fb0
Patrick Selin <patrick.selin@gmail.com>
2024-08-26 10:59:01 +0300

    drizzle.congig.ts and migrate.ts

---
+++ server/migrations/0000_grey_stellaris.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS "test_items" (
+	"id" varchar(255) PRIMARY KEY NOT NULL,
+	"content" varchar(255),
+	"important" boolean
+);
--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,7 +1,13 @@
-import { pgTable, varchar, boolean } from "drizzle-orm/pg-core";
+import { pgTable, varchar, boolean, serial, text } from "drizzle-orm/pg-core";
 
-export const testItems = pgTable("test_items", {
+export const testItems = pgTable("test_items_two", {
   id: varchar("id", { length: 255 }).primaryKey(),
   content: varchar("content", { length: 255 }),
   important: boolean("important"),
+});
+
+export const users = pgTable("test_users", {
+  id: serial("id").primaryKey(),
+  fullName: text("full_name"),
+  phone: varchar("phone", { length: 256 }),
 });
