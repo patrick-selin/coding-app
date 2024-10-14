8e19918b3a36610f54198d17025315df17fcc0d4
Patrick Selin <patrick.selin@gmail.com>
2024-10-14 14:36:40 +0300

    fix dev nginx setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 // index.ts
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import app from './server';
+import app from "./server";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
