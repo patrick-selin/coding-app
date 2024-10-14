4ce5b2b384cc86799e076b03c86f3b53d011d1a0
Patrick Selin <patrick.selin@gmail.com>
2024-10-14 15:22:02 +0300

    cleanup code

--- client/src/services/test-service.ts
+++ client/src/services/test-service.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// const baseUrl: string = "http://server:3333/api1/";
-// const baseUrl = "http://localhost:3333/api1/test";
 const baseUrl = `${import.meta.env.VITE_BASE_URL}/test`;
 console.log(baseUrl);
 
