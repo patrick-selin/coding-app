685282ddf29950e6800b9c4265b8bc39fb268929
Patrick Selin <patrick.selin@gmail.com>
2024-10-15 07:17:32 +0300

    setup dotenv to handle different environment files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,18 @@
 // index.ts
 import dotenv from "dotenv";
-dotenv.config();
-
-
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+// tee typescript def for env files
 import app from "./server";
-// import config from './config/env-config';
-
 console.log(`NODE_ENV=${process.env.NODE_ENV}`);
 
 const PORT = parseInt(process.env.PORT || "3334", 10);
 
-console.log(process.env.port);
+console.log(`Port is : ${process.env.PORT}`);
+console.log(`Path is : ${process.env.PATH}`);
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Environment mode: ++${process.env.NODE_ENV}++`);
 });
