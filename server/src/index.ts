57d23cec2cffafa2bb00a78e2339b7b83c1c1f0a
Patrick Selin <patrick.selin@gmail.com>
2024-10-17 15:23:00 +0300

    remove hardcoded host -> env

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,9 @@
 import { config } from "./config/config";
 import app from "./server";
 
-app.listen(config.PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port: ++${config.PORT}++`);
+app.listen(config.SERVER_PORT, config.SERVER_HOST, () => {
+  console.log(
+    `Server is running on port: ++${config.SERVER_PORT}, host: ${config.SERVER_HOST}++`
+  );
   console.log(`Environment mode: ++${process.env.NODE_ENV}++`);
 });
