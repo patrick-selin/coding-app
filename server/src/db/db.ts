b315a7f5c3b6e19c21d3bc945610edd818155e51
Patrick Selin <patrick.selin@gmail.com>
2024-11-13 14:13:49 +0200

    mock db ts error issue

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -4,11 +4,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 import { config } from "../config/config";
+import { DbClient } from "../../types/db";
 
 const databaseUrl =
   config.NODE_ENV === "test" ? config.TEST_DATABASE_URL : config.DATABASE_URL;
-  console.log(`db url :: ${databaseUrl}`);
+console.log(`db url :: ${databaseUrl}`);
 const queryClient = postgres(databaseUrl);
 console.log(`Connecting to database: ${config.POSTGRES_DB}`);
 
-export const db = drizzle(queryClient, { schema, logger: true });
+export const db: DbClient = drizzle(queryClient, { schema, logger: true });
