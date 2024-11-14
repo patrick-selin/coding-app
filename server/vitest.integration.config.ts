6d09da6cc9a4873548dc9e156f30c88881415843
Patrick Selin <patrick.selin@gmail.com>
2024-11-14 09:48:14 +0200

    yarn issue

--- server/vitest.integration.config.ts
+++ server/vitest.integration.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: ["SRC/tests/integration/*.test.ts"],
-    exclude: ["src/**/*.test.ts"],
+    // include: ["/usr/src/app/src/tests/integration/*.test.ts"],
+    include: ["/usr/src/app/build/src/tests/integration/**/*.test.js"],
   },
 });
