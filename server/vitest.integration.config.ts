f7faede0dfd180cb1740bdbdc592a870e83a92ca
Patrick Selin <patrick.selin@gmail.com>
2024-11-19 12:46:47 +0200

    ci: update integration test flow

--- server/vitest.integration.config.ts
+++ server/vitest.integration.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    // include: ["/usr/src/app/src/tests/integration/*.test.ts"],
-    include: ["/usr/src/app/build/src/tests/integration/**/*.test.js"],
+    include: ["src/tests/integration/*.test.ts"],
+    // include: ["/usr/src/app/build/src/tests/integration/**/*.test.js"],
   },
 });
