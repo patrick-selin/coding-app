3ad9c04529f208ff758637847fedb9a3915f8627
Patrick Selin <patrick.selin@gmail.com>
2024-11-19 13:40:48 +0200

    terraform: init tf, add aws credentials/temp ec2 instance

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.73"
+      source = "hashicorp/aws"
+      version = "5.76.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.9.7"
 }
 
 provider "aws" {
-  region  = "eu-north-1"
+  region = "eu-north-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-xxx"
+resource "aws_instance" "coding_app_server" {
+  ami           = "ami-0658158d7ba8fd573"
   instance_type = "t3.micro"
 
   tags = {
