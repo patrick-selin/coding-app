b14caa127e7888fc5230229a8cfe46b8d45f751a
Patrick Selin <patrick.selin@gmail.com>
2024-12-04 07:58:51 +0200

    add react router for the App-component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,16 @@
+// main.tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+// import { MantineProvider } from "@mantine/core";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
