a94d0e1aa1773141d9b9f752ddf07a6bfb350892
Patrick Selin <patrick.selin@gmail.com>
2024-12-28 11:22:13 +0200

    fix: lint

---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+// pages/dashboard.tsx
+import { Title, Text } from "@mantine/core";
+function Dashboard() {
+  return (
+    <div>
+      <Title order={2}>Dashboard</Title>
+      <Text size="md">Welcome to your Dashboard!</Text>
+    </div>
+  );
+}
+
+export default Dashboard;
